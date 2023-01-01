689906bb4e0661daf127f474ed65828305322e46
houariDz95 <houarieddrief7@gmail.com>
2023-01-01 18:11:41 +0100

    fix app

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,4 +1,3 @@
-'use client';
 import { motion } from "framer-motion";
 import { textVariant2, textContainer } from "../utils/motion";
 export const TypingText = ({title, textStyles}) => (
--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,4 +1,3 @@
-'use client';
 import { motion } from "framer-motion";
 import { fadeIn } from "../utils/motion";
 const InsightCard = ({imgUrl, title, subtitle, index}) => (
