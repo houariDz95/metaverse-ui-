6397d8d8f0d4dc663932bb8a32ef5cc5fc38cfdd
houariDz95 <houarieddrief7@gmail.com>
2023-01-02 11:46:34 +0100

    first commit

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,10 +1,10 @@
 import { motion} from 'framer-motion';
 import styles from '../styles';
-import {fadeIn} from '../utils/motion'
+import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({id, imgUrl, title, index, active, handelClick}) => (
   <motion.div
-    variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+    variants={ fadeIn('right', 'spring', index * 0.5, 0.75) }
     className={`relative ${active === id ?
        'lg:flex-[3.5] flex-[10]' :
         'lg:flex-[0.5] flex-[2]'} 
@@ -18,11 +18,11 @@ const ExploreCard = ({id, imgUrl, title, index, active, handelClick}) => (
         lg:rotate-[-90deg] lg:origin-[0,0]"> {title}</h3>
       ) : (
         <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
-          <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px] `}>
+          <div className={ `${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px] ` }>
             <img src='/headset.svg' alt="headset" className='w-1/2 h-1/2 object-contain'/>
           </div>
           <p className="font-normal text-[16px] leading-[20px] text-white uppercase">Enter the Metaverse</p>
-          <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">{title}</h2>
+          <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">{ title }</h2>
         </div>
 
       )}
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,17 @@
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import { socials } from  '../constants';
 import styles from '../styles';
 import { footerVariants} from '../utils/motion';
 
 const Footer = () => (
   <motion.footer
-  variants={footerVariants}
+  variants={ footerVariants } 
   initial="hidden"
   whileInView="show"
-  className={`${styles.paddings} py-8 relative`}
+  className={`${ styles.paddings}  py-8 relative`}
   >
     <div className="footer-gradient" />
-    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+    <div className={ `${styles.innerWidth} mx-auto flex flex-col gap-8` }>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white ">Enter the Metaverse</h4>
         <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px]">
